144e6e4feba01879699dd1f57d48e4d5209e2ae8
Alexander Kimpel <Alex@lawn-128-61-127-55.lawn.gatech.edu>
2018-02-27 11:29:28 -0500

    Initial Commit v2

--- src/Ringo.java
+++ src/Ringo.java
@@ -1,5 +1,44 @@
+import java.util.Vector;
+
 /**
  * Created by Katie on 2/27/18.
  */
 public class Ringo {
+
+    public int num;
+    public String ID;
+    public final int PORT_NUMBER = 5600;
+    public final String IP_ADDR = "127.0.0.1"; //TODO: Change from Local
+    public Vector<Double> RTT;
+    public Vector<Vector<Double>> RTTMatrix;
+
+
+    public static void main(String[] args) {
+        //TODO: Create command line parsing arguments
+
+        //TODO: Initialize Ringo Timer
+        //TODO: Initialize RTT Vector.
+    }
+
+    public void pointOfContact() {
+        //TODO: Attach this Ringo to any other Ringos in the network.
+        //TODO: Obtain list of all other Ringos in the network.
+    }
+
+    public void sendDummy() {
+        //TODO: Send a dummy packet to other Ringos in the network to see if they are alive.
+        //Might not be in Milestone 2.
+    }
+
+    public void recieveDummy() {
+        //TODO: Ping other ringos in the network. Calculate RTT through timestamp.
+    }
+
+    public void floydWarshall() {
+        //TODO: Construct shortest path among all Ringos in the network.
+    }
+
+    public void sendPacket(Ringo r) {
+        //TODO: Send a packet to another Ringo in the network.
+    }
 }
